75568360f41ef968257809f6674908f1c65d7efc
ra890927 <happy.890927410@gmail.com>
2020-11-13 15:56:54 +0800

    Binary Tree

--- Binary_Tree_Formula/Binary_Tree_Formula.cpp
+++ Binary_Tree_Formula/Binary_Tree_Formula.cpp
@@ -2,40 +2,98 @@
 using namespace std;
 
 struct Node{
-	char op = 0;
+	char op;
 	int value;
 	Node *left, *right;
 };
 
-vector<int> cor;
+bool is_error = false;
 
 bool is_operator(char c){
 	if(c == '+' || c == '-' || c == '*' || c == '/') return true;
 	return false;
 }
 
-Node *build_leaf(string s){
+bool is_number(char c){
+	if(c >= '0' && c <= '9') return true;
+	return false;
+}
+
+bool oper_priority(char a, char b){
+	if(!b) return true;
+
+	if(a == '+' || a == '-') return true;
+	else if(a == '*' || a == '/'){
+		if(b == '*' || b == '/') return true;
+		else return false;
+	}
+
+	return false;
+}
+
+Node *node_init(){
+	Node *temp = new Node;
+	temp -> op = 0;
+	temp -> value = 0;
+	temp -> left = NULL;
+	temp -> right = NULL;
+	return temp;
+}
+
+Node *build_leaf(string s, bool is_signed){
 	int64_t num = 0;
-	for(int64_t i = 0; i < s.length(); i++){
+	for(int64_t i = is_signed & 1; i < s.length(); i++){
 		num *= 10;
 		num += s[i] - '0';
 	}
 
-	Node *temp = new Node;
-	temp -> value = num;
-	temp -> left = NULL;
-	temp -> right = NULL;
+	Node *temp = node_init();
+	if(is_signed) temp -> value = -num;
+	else temp -> value = num;
+
 	return temp;
 }
 
 Node *build_node(string s){
 	Node *temp = new Node;
 
+	int sub_i = -1;
+	int sub_cnt = 0;
 	bool check = true;
-	for(int64_t i = 0; i < s.length(); i++)
-		if(is_operator(s[i])) check = false;
-	if(check) return build_leaf(s);
+	for(int64_t i = 1; i < s.length(); i++){
+		if(is_operator(s[i])){
+			check = false;
+			if(s[i] == '-'){
+				sub_cnt += 1;
+				sub_i = i;
+			}
+		}
+	}
 
+	if(sub_cnt == 1 && sub_i == 1) check = true;
+
+	cout << s << endl;
+
+	if(check){
+		if(s[0] == '-') return build_leaf(s, true);
+		else if(is_number(s[0])) return build_leaf(s, false);
+		else if(s[0] == '(' && s[s.length() - 1] == ')'){
+			s.assign(s, 1, s.length() - 2);
+
+			if(s[0] == '-') temp = build_leaf(s, true);
+			else if(is_number(s[1])) temp = build_leaf(s, false);
+
+			return temp;
+		}
+		else{
+			is_error = true;
+			printf("Illegal input.\n");
+			return node_init();
+		}
+	}
+
+	char it = 0;
+	int index = 0;
 	stack<char> sta;
 	for(int64_t i = 0; i < s.length(); i++){
 		if(s[i] == '(') sta.push('(');
@@ -43,20 +101,31 @@ Node *build_node(string s){
 			if(sta.top() == '(') sta.pop();
 			else sta.push(')');
 		}
-		else if(sta.empty() && is_operator(s[i])){
-			temp->op = s[i];
-			string a, b;
-			a.assign(s, 0, i);
-			b.assign(s, i + 1, s.length());
-			if(a[0] == '(' && a[a.length() - 1] == ')')
-				a.assign(a, 1, a.length() - 2);
-			if(b[0] == '(' && b[b.length() - 1] == ')')
-				b.assign(b, 1, b.length() - 2);
-			cout << a << " " << b << endl;
-			temp -> left = build_node(a);
-			temp -> right = build_node(b);
+		else if(sta.empty() && is_operator(s[i]) && oper_priority(s[i], it)){
+			it = s[i];
+			index = i;
 		}
 	}
+
+	if(is_operator(it)){
+		temp->op = s[index];
+
+		string a, b;
+		a.assign(s, 0, index);
+		b.assign(s, index + 1, s.length());
+
+		if(a[0] == '(' && a[a.length() - 1] == ')')
+			a.assign(a, 1, a.length() - 2);
+		if(b[0] == '(' && b[b.length() - 1] == ')')
+			b.assign(b, 1, b.length() - 2);
+
+		cout << a << " " << b << endl;
+
+		temp -> left = build_node(a);
+		temp -> right = build_node(b);
+	}
+
+	cout << "\n\n";
 
 	return temp;
 }
@@ -76,7 +145,8 @@ int main(){
 
 	string formula;
 	while(cin >> formula){
-		Node *head =  build_node(formula);
+		Node *head = build_node(formula);
 		posorder_traversal(head);
+		printf("\n");
 	}
 }
--- Binary_Tree_Formula/test2.cpp
+++ Binary_Tree_Formula/test2.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main(){
 	string s;
 	cin >> s;
-	for(int i = 0; i < s.length(); i++)
-		if(s[i] == '+') s.assign(s, i + 1, s.length());
+	s.assign(s, 0, s.length() - 1);
 	cout << s << endl;
 }
