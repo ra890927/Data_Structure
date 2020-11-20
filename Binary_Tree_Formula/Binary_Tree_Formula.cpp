e7d57798716f30f71cd4b3b648499519338435f4
ra890927 <happy.890927410@gmail.com>
2020-11-20 18:33:04 +0800

    Binary Tree

--- Binary_Tree_Formula/Binary_Tree_Formula.cpp
+++ Binary_Tree_Formula/Binary_Tree_Formula.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 struct Node{
 	char op;
-	int value;
+	double value;
 	Node *left, *right;
 };
 
@@ -29,6 +29,21 @@ bool oper_priority(char a, char b){
 	}
 
 	return false;
+}
+
+double compute_node(double a, double b, char op){
+	switch(op){
+		case '+':
+			return a + b;
+		case '-':
+			return a - b;
+		case '*':
+			return a * b;
+		case '/':
+			return a / b;
+	}
+
+	return 0;
 }
 
 Node *node_init(){
@@ -72,7 +87,7 @@ Node *build_node(string s){
 
 	if(sub_cnt == 1 && sub_i == 1) check = true;
 
-	cout << s << endl;
+	//cout << s << endl;
 
 	if(check){
 		if(s[0] == '-') return build_leaf(s, true);
@@ -119,24 +134,71 @@ Node *build_node(string s){
 		if(b[0] == '(' && b[b.length() - 1] == ')')
 			b.assign(b, 1, b.length() - 2);
 
-		cout << a << " " << b << endl;
+		//cout << a << " " << b << endl;
 
 		temp -> left = build_node(a);
 		temp -> right = build_node(b);
+		temp -> value = compute_node(temp -> left -> value, temp -> right -> value, temp -> op);
 	}
 
-	cout << "\n\n";
+	//cout << "\n\n";
 
 	return temp;
+}
+
+Node *delete_node(Node *root){
+	if(root -> left) root -> left = delete_node(root -> left);
+	if(root -> right) root -> right = delete_node(root -> right);
+	delete root;
+	return NULL;
 }
 
 void posorder_traversal(Node *root){
 	if(root){
 		posorder_traversal(root -> left);
 		posorder_traversal(root -> right);
-		if(!root -> op) printf("%d ", root -> value);
+		if(!root -> op) printf("%.0lf ", root -> value);
 		else printf("%c ", root -> op);
 	}
+}
+
+void preorder_traversal(Node *root){
+	if(root){
+		if(!root -> op) printf("%.0lf ", root -> value);
+		else printf("%c ", root -> op);
+		preorder_traversal(root -> left);
+		preorder_traversal(root -> right);
+	}
+}
+
+void inorder_traversal(Node *root){
+	if(root){
+		preorder_traversal(root -> left);
+		if(!root -> op) printf("%.0lf ", root -> value);
+		else printf("%c ", root -> op);
+		preorder_traversal(root -> right);
+	}
+}
+
+void traversal_node(Node *root, int mode){
+	if(mode == 1){
+		printf("The inorder traversal is : ");
+		inorder_traversal(root);
+	}
+	if(mode == 2){
+		printf("The posorder traversal is : ");
+		posorder_traversal(root);
+	}
+	if(mode == 3){
+		printf("The preorder traversal is : ");
+		preorder_traversal(root);
+	}
+
+	printf("\n");
+}
+
+void check_formula(string s){
+	
 }
 
 int main(){
@@ -144,9 +206,24 @@ int main(){
 	cin.tie(0);
 
 	string formula;
-	while(cin >> formula){
+	while(true){
+		system("cls");
+		printf("Please input the formula.\n");
+		cin >> formula;
+
+		check_formula(formula);
+
 		Node *head = build_node(formula);
-		posorder_traversal(head);
+		
 		printf("\n");
+		traversal_node(head, 2);
+		printf("\n");
+		
+		printf("The answer is : %lf\n\n", head -> value);
+
+		delete_node(head);
+		system("pause");
 	}
+
+	return 0;
 }
